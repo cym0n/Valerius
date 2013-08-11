cfe0ee1ef683f7a5d97d18d56a0d1c7c996bf921
cym0n <cymon@tiscali.it>
2013-08-11 14:48:52 +0200

    List and novel page refactored

--- src/lib/Valerius.pm
+++ src/lib/Valerius.pm
@@ -9,51 +9,28 @@ use Data::Dumper;
 set layout => 'valerius';
 
 get '/' => sub {
-    my $category_news = schema->resultset('Category')->find( { category => 'notizie' } );
-    my @news = $category_news->articles->search( { published => 1 }, { order_by => { -desc => 'publish_date' } });
-    my $news_data = $news[0]->contents->find({ language => 'it' });
+    my $news = Strehler::Element::Article::get_last_by_date('notizie');
+    my $chapter = Strehler::Element::Article::get_last_by_order('romanzo');
+    my %news_data = $news->get_ext_data('it');
 
-    my $category_novel = schema->resultset('Category')->find( { category => 'romanzo' } );
-    my @chapters = $category_novel->articles->search( { published => 1 }, { order_by => { -desc => 'display_order' } });
-    my $chapter_data = $chapters[0]->contents->find({ language => 'it' });
-    
-    my %article;
-    $article{'title'} = $news_data->title;
-    $article{'text'} = markdown($news_data->text);
-    $article{'publish_date'} = $news[0]->publish_date->strftime('%d-%m-%Y');
-    
-    my %extract;
-    $extract{'title'} = $chapter_data->title;
-    $extract{'text'} = markdown(substr($chapter_data->text, 0, 800) . "...");
-    $extract{'chapter_number'} = $chapters[0]->display_order;
-    $extract{'link'} = '/novel/' . $chapter_data->slug;
-    
-    template "index", { article => \%article, chapter => \%extract };
+    $news_data{'publish_date'} = $news_data{'publish_date'}->strftime('%d-%m-%Y');
+    my %chapter_data = $chapter->get_ext_data('it');
+    $chapter_data{'text'} = markdown(substr($chapter_data{'text'}, 0, 800) . "...");
+
+    template "index", { article => \%news_data, chapter => \%chapter_data };
 };
 
 get '/novel' => sub {
     my $entries_per_page = 20;
     my $page = params->{page} || 1;
     my $order = params->{order} || 'desc';
-    my $category_novel = schema->resultset('Category')->find( { category => 'romanzo' } );
-    my $rs = $category_novel->articles->search({ published => 1 }, { order_by => { '-' . $order => 'display_order' } , page => 1, rows => $entries_per_page});
-    my $pager = $rs->pager();
-    my $elements = $rs->page($page);
-    my @chapters;
-    for($elements->all())
-    {
-        my $el = $_;
-        my %chap;
-        $chap{'number'} = $el->display_order;
-        $chap{'title'} = $el->contents->find({ language => 'it' })->title;
-        $chap{'slug'} = $el->contents->find({ language => 'it' })->slug;
-        push @chapters, \%chap;
-    }
-
-    template "novel", { chapters => \@chapters, page => $page, order => $order, last_page => $pager->last_page() };
+    my $elements = Strehler::Element::Article::get_list({ page => $page, entries_per_page => $entries_per_page, category => 'romanzo', language => 'it', ext => 1, published => 1});
+    template "novel", { chapters => $elements->{'to_view'}, page => $page, order => $order, last_page => $elements->{'last_page'} };
+    #template "novel", { chapters => \@chapters, page => $page, order => $order, last_page => $pager->last_page() };
 };
 
 get '/novel/last-chapter' => sub {
+    my $category = Strehler::Element::Category->new(name => 'romanzo');
     my $category_novel = schema->resultset('Category')->find( { category => 'romanzo' } );
     my @chapters = $category_novel->articles->search( { published => 1 }, { order_by => { -desc => 'display_order' } });
     my $chapter_data = $chapters[0]->contents->find({ language => 'it' });
